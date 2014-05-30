1dce9680711aef6c71264b89d25b39acf6ab41d4
Tim Soethout <tim@glukorizon.nl>
2014-05-30 17:47:29 +0200

    Added property based tests

--- code/src/test/scala/Example1Test.scala
+++ code/src/test/scala/Example1Test.scala
@@ -1,8 +1,31 @@
+import Example1._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{Matchers, FlatSpec}
 
 class Example1Test extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
+  behavior of "reverseStrings"
 
-  "reverseStrings" should "reverse a string" in {
-    Example1.reverseStrings(List("A", "list", "of", "Strings")) should equal List("Strings", "of", "list", "A")
+  it should "reverse a string" in {
+    val reversed = reverseStrings(List("A", "list", "of", "Strings"))
+    reversed should equal(List("Strings", "of", "list", "A"))
+  }
+
+  it should "also reverse all strings" in {
+    forAll {
+      (ss: List[String]) => reverseStrings(ss) should be(ss.reverse) // should result in the same as library implementation
+    }
+  }
+
+  it should "have the same size" in {
+    forAll {
+      (ss: List[String]) => reverseStrings(ss) should have length (ss.length)
+    }
+  }
+
+  it should "have the same elements" in {
+    forAll {
+      (ss: List[String]) => reverseStrings(ss) should contain theSameElementsAs (ss)
+    }
   }
 
 }
