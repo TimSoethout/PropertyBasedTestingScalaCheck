bd0fa8025bbeeea4acf6f88daf3d7f10e97b139e
Tim Soethout <tim@glukorizon.nl>
2014-06-01 16:47:20 +0200

    Fixed ScalaCheck property using boolean premise

--- code/src/test/scala/pbt/PropertiesTest.scala
+++ code/src/test/scala/pbt/PropertiesTest.scala
@@ -2,13 +2,14 @@ package pbt
 
 import IbanExample._
 import org.scalacheck._
-import Prop.forAll
+import org.scalacheck.Prop.{forAll, BooleanOperators}
 
 object PropertiesTest extends Properties("IbanExample") {
 
   property("contain the origin bban") =
     forAll {
       bban: Int =>
-        calculateIban(bban.toString) contains bban.toString
+        (bban > 0) ==>
+          (calculateIban(bban.toString) contains bban.toString)
     }
 }
